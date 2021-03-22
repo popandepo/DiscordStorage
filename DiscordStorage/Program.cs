4d578f6271a16f8e7acf201d43a0730fa11b3f27
popandepo <popandepo@gmail.com>
2021-03-22 16:24:20 +0100

    Did a code cleanup

--- DiscordStorage/Program.cs
+++ DiscordStorage/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace DiscordStorage
+﻿namespace DiscordStorage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
