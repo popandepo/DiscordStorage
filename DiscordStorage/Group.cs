764e046f9e4d15d043bf90b958e03feb0baec91a
popandepo <popandepo@gmail.com>
2021-03-22 14:58:36 +0100

    Added User, Group and Information class

---
+++ DiscordStorage/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordStorage
+{
+    class Group
+    {
+        ulong ID { get; set; }
+        List<User> Users { get; set; }
+        List<Information> Info { get; set; }
+    }
+}
